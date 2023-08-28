0c66a58383bf748d552ce9dd4df066d01b2488aa
ianshaloom <ianshaloom0@outlook.com>
2023-08-28 10:46:30 +0300

    new data library

---
+++ lib/data/data.dart
@@ -0,0 +1,35 @@
+import '../models/note.dart';
+
+final List<Note> notes = [
+  Note(
+      title: 'Product Management',
+      date: DateTime.now(),
+      note:
+          """Product management is a strategic role within a company that involves overseeing the development and management of a product or suite of products throughout their lifecycle. The primary goal of a product manager is to create and deliver products that meet customer needs, align with business objectives, and generate value for the organization.
+Market research and analysis: Product managers conduct market research to understand customer needs, preferences, and trends. They analyze market data and competitive landscape to identify opportunities and make informed product decisions.
+Product strategy: Product managers define the product vision, goals, and strategy based on market research and business objectivesI. Inefficient Expense Tracking: Without an effective system for tracking expenses, young business owners may struggle to monitor their business costs accurately.
+This can lead to overspending, financial instability, and difficulty in identifying areas for cost-cutting.\n\n
+Product management is an organizational function that guides every step of a product’s lifecycle — from development to positioning and pricing — by focusing on the product and its customers first and foremost. To build the best possible product, product managers advocate for customers within the organization and make sure the voice of the market is heard and heeded.
+
+Thanks to this focus on the customer, product teams routinely ship better-designed and higher-performing products. In tech, where entrenched products are quickly uprooted by newer and better solutions, there is more need than ever for an intimate understanding of customers and the ability to create tailored solutions for them. That’s where product management comes in."""),
+  Note(
+      title: 'Tommorrows ToDo List',
+      date: DateTime.now(),
+      note:
+          "Inefficient Expense Tracking: Without an effective system for tracking expenses, young business owners may struggle to monitor their business costs accurately. This can lead to overspending, financial instability, and difficulty in identifying areas for cost-cutting."),
+  Note(
+      title: 'Environment Club Metting',
+      date: DateTime.now(),
+      note:
+          "Limited Profit and Loss Insights: Due to the absence of tools to calculate and analyze profit and loss, these entrepreneurs might not have a clear understanding of their business's financial health. This lack of insight can hinder their ability to make informed decisions and plan for the future."),
+  Note(
+      title: 'GitHub Commits',
+      date: DateTime.now(),
+      note:
+          "Inadequate Business Decision-Making: Without access to business data and analytics, the youth entrepreneurs may struggle to make sound decisions. This can result in missed opportunities, poor resource allocation, and less efficient business strategies"),
+  Note(
+      title: 'Agile Deveopment',
+      date: DateTime.now(),
+      note:
+          "Financial Literacy Gap: Many youth entrepreneurs may not have a solid understanding of financial concepts, making it challenging for them to manage their business finances effectively. Your app can bridge this gap by providing educational resources and insights related to financial management and business operations."),
+];
--- lib/pages/noteshomepage/notes_home_page.dart
+++ lib/pages/noteshomepage/notes_home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:yournotes/pages/dialogs/note_view_dialog.dart';
 
+import '../../data/data.dart';
 import '../../models/note.dart';
 import '../../widgets/disable_list_glow.dart';
 import '../navigationpage/navigation_drawer.dart';
@@ -104,38 +105,5 @@ class _NotesHomePageState extends State<NotesHomePage> {
     );
   }
 
-  final List<Note> notes = [
-    Note(
-        title: 'Product Management',
-        date: DateTime.now(),
-        note:
-            """Product management is a strategic role within a company that involves overseeing the development and management of a product or suite of products throughout their lifecycle. The primary goal of a product manager is to create and deliver products that meet customer needs, align with business objectives, and generate value for the organization.
-Market research and analysis: Product managers conduct market research to understand customer needs, preferences, and trends. They analyze market data and competitive landscape to identify opportunities and make informed product decisions.
-Product strategy: Product managers define the product vision, goals, and strategy based on market research and business objectivesI. Inefficient Expense Tracking: Without an effective system for tracking expenses, young business owners may struggle to monitor their business costs accurately.
-This can lead to overspending, financial instability, and difficulty in identifying areas for cost-cutting.\n\n
-Product management is a strategic role within a company that involves overseeing the development and management of a product or suite of products throughout their lifecycle. The primary goal of a product manager is to create and deliver products that meet customer needs, align with business objectives, and generate value for the organization.
-Market research and analysis: Product managers conduct market research to understand customer needs, preferences, and trends. They analyze market data and competitive landscape to identify opportunities and make informed product decisions.
-Product strategy: Product managers define the product vision, goals, and strategy based on market research and business objectivesI. Inefficient Expense Tracking: Without an effective system for tracking expenses, young business owners may struggle to monitor their business costs accurately.
-This can lead to overspending, financial instability, and difficulty in identifying areas for cost-cutting."""),
-    Note(
-        title: 'Tommorrows ToDo List',
-        date: DateTime.now(),
-        note:
-            "Inefficient Expense Tracking: Without an effective system for tracking expenses, young business owners may struggle to monitor their business costs accurately. This can lead to overspending, financial instability, and difficulty in identifying areas for cost-cutting."),
-    Note(
-        title: 'Environment Club Metting',
-        date: DateTime.now(),
-        note:
-            "Limited Profit and Loss Insights: Due to the absence of tools to calculate and analyze profit and loss, these entrepreneurs might not have a clear understanding of their business's financial health. This lack of insight can hinder their ability to make informed decisions and plan for the future."),
-    Note(
-        title: 'GitHub Commits',
-        date: DateTime.now(),
-        note:
-            "Inadequate Business Decision-Making: Without access to business data and analytics, the youth entrepreneurs may struggle to make sound decisions. This can result in missed opportunities, poor resource allocation, and less efficient business strategies"),
-    Note(
-        title: 'Agile Deveopment',
-        date: DateTime.now(),
-        note:
-            "Financial Literacy Gap: Many youth entrepreneurs may not have a solid understanding of financial concepts, making it challenging for them to manage their business finances effectively. Your app can bridge this gap by providing educational resources and insights related to financial management and business operations."),
-  ];
+
 }
